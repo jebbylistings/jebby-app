51e177214c7a5d28d9ae33f313453898ced2a70b
figletechnologies <94354955+figletechnologies@users.noreply.github.com>
2026-01-11 20:24:23 +0530

    11january

---
+++ lib/model/DataModel.dart
@@ -0,0 +1,82 @@
+
+class Data {
+  int? id;
+  int? userId;
+  int? categoryId;
+  int? subcategoryId;
+  String? name;
+  int? price;
+  String? specifications;
+  String? serviceAgreements;
+  String? createdAt;
+  String? updatedAt;
+  int? negotiation;
+  int? isMessage;
+  int? isReview;
+  String? stars;
+  String? length;
+  String? image;
+  String? delivery_charges;
+
+  Data({
+  this.id,
+  this.userId,
+  this.categoryId,
+  this.subcategoryId,
+  this.name,
+  this.price,
+  this.specifications,
+  this.serviceAgreements,
+  this.createdAt,
+  this.updatedAt,
+  this.negotiation,
+  this.isMessage,
+  this.isReview,
+  this.stars,
+  this.length,
+  this.image,
+  this.delivery_charges,
+});
+
+Data.fromJson(Map<String, dynamic> json) {
+id = json['id'];
+userId = json['user_id'];
+categoryId = json['category_id'];
+subcategoryId = json['subcategory_id'];
+name = json['name'];
+price = json['price'];
+specifications = json['specifications'];
+serviceAgreements = json['service_agreements'];
+createdAt = json['created_at'];
+updatedAt = json['updated_at'];
+negotiation = json['negotiation'];
+isMessage = json['isMessage'];
+isReview = json['is_review'];
+stars = json['stars'];
+length = json['length'];
+image = json['image'];
+delivery_charges = json['delivery_charges'];
+}
+
+Map<String, dynamic> toJson() {
+final Map<String, dynamic> data = new Map<String, dynamic>();
+data['id'] = this.id;
+data['user_id'] = this.userId;
+data['category_id'] = this.categoryId;
+data['subcategory_id'] = this.subcategoryId;
+data['name'] = this.name;
+data['price'] = this.price;
+data['specifications'] = this.specifications;
+data['service_agreements'] = this.serviceAgreements;
+data['created_at'] = this.createdAt;
+data['updated_at'] = this.updatedAt;
+data['negotiation'] = this.negotiation;
+data['isMessage'] = this.isMessage;
+data['is_review'] = this.isReview;
+data['stars'] = this.stars;
+data['length'] = this.length;
+data['image'] = this.image;
+data['delivery_charges'] = this.delivery_charges;
+return data;
+}
+}
